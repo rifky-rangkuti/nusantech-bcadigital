bb897d9ffb2133e29813d38fff2e10eeaa21d256
Rifky Rangkuti <rifky.rangkuti@gmail.com>
2025-05-16 03:00:33 +0700

    fix: prev disabled

--- src/app/movies/Pagination.client.tsx
+++ src/app/movies/Pagination.client.tsx
@@ -5,6 +5,7 @@ import React from "react";
 import { useFilterStore } from "~/stores/filter.store";
 
 export default function Pagination() {
+  const page = useFilterStore((s) => s.page);
   const prevPage = useFilterStore((s) => s.prevPage);
   const nextPage = useFilterStore((s) => s.nextPage);
   return (
@@ -12,7 +13,7 @@ export default function Pagination() {
       <button
         onClick={() => prevPage()}
         className="flex-1 h-10 rounded-lg bg-satu text-sm font-semibold text-white disabled:bg-gray-300"
-        disabled={true}
+        disabled={page === 1}
       >
         Previous
       </button>
